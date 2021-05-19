a8d6cefb511d828037f1dc0b133175517b21e269
parvez09ahmad <parveziahmad09@gmail.com>
2021-05-19 13:02:46 +0530

    added rest api code

--- SpringBoot_Branching/src/main/java/com/javatechie/test/SpringBootBranchingApplication.java
+++ SpringBoot_Branching/src/main/java/com/javatechie/test/SpringBootBranchingApplication.java
@@ -2,11 +2,19 @@ package com.javatechie.test;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
 public class SpringBootBranchingApplication {
+
+	@GetMapping("/{purchase}/{username}/{amount}/{productName}")
+	public String Purchase(@PathVariable String username, @PathVariable double amount,
+			@PathVariable String productName) {
+		return "hi " + username + " order for " + productName + " with amount " +amount+ " Stored amount";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootBranchingApplication.class, args);
