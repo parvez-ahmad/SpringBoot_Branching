e268d8cee824a9012f34647c7cf842c550e3c612
parvez09ahmad <parveziahmad09@gmail.com>
2021-05-19 12:12:34 +0530

    First Commit

---
+++ SpringBoot_Branching/src/main/java/com/javatechie/test/SpringBootBranchingApplication.java
@@ -0,0 +1,13 @@
+package com.javatechie.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootBranchingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootBranchingApplication.class, args);
+	}
+
+}
